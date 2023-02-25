b723bf9565aac684303623839b350471c0e46c9a
Miles-Cownie <miles_cownie94@protonmail.com>
2023-02-25 14:43:30 +0000

    Modify settings to fix bootstrap crispyforms conflict

--- nacho_emporium/settings.py
+++ nacho_emporium/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'checkout',
     # Other
     'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -172,8 +173,12 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Stripe
 FREE_DELIVERY_THRESHOLD = 40
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
