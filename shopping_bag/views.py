5287d9eaa670db3b48bc086746c61439c9220ca7
Miles-Cownie <miles_cownie94@protonmail.com>
2023-02-25 10:51:57 +0000

    Split bag html in includes and fix js remove item bug

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -11,7 +11,7 @@ def view_bag(request):
     A view that renders the bag contents page
     """
 
-    return render(request, 'bag.html')
+    return render(request, 'bag/bag.html')
 
 
 def add_to_bag(request, item_id):
@@ -60,7 +60,9 @@ def add_to_bag(request, item_id):
 
 
 def adjust_bag(request, item_id):
-    """Adjust the quantity of the specified product to the specified amount"""
+    """
+    Adjust the quantity of the specified product to the specified amount
+    """
 
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
@@ -100,7 +102,9 @@ def adjust_bag(request, item_id):
 
 
 def remove_from_bag(request, item_id):
-    """Remove the item from the shopping bag"""
+    """
+    Remove the item from the shopping bag
+    """
 
     try:
         product = get_object_or_404(Product, pk=item_id)
